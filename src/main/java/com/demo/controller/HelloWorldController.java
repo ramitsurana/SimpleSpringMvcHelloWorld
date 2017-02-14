8c7ed67e95303991bb154c481678f033545c1318
Abir Datta <abirdatta.in@gmail.com>
2017-02-14 04:22:31 +0000

    initial commit for a simple Hello World web app using spring mvc

---
+++ src/main/java/com/demo/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.demo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	String message = "Hello World!";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+
+		ModelAndView mv = new ModelAndView("helloworld", "message", message);
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
